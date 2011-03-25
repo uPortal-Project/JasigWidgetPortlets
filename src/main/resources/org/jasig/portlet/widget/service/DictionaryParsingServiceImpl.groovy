e0f22a40e802f693fb9eb28058872fe052254792
Jen Bourey <jennifer.bourey@gmail.com>
2011-03-25 16:24:18 +0000

    WIDGPT-6 Updating license headers.
    git-svn-id: https://source.jasig.org/portlets/JasigWidgetPortlets/trunk@23244 f5dbab47-78f9-eb45-b975-e544023573eb

--- src/main/resources/org/jasig/portlet/widget/service/DictionaryParsingServiceImpl.groovy
+++ src/main/resources/org/jasig/portlet/widget/service/DictionaryParsingServiceImpl.groovy
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 import java.io.InputStream
 import groovy.xml.StreamingMarkupBuilder
 import org.jasig.portlet.widget.service.IDictionaryParsingService
