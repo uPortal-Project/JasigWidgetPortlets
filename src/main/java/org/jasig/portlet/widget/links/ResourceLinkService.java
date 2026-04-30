8a514ee4a159646a9e3ac72867d40b8be2ba07f6
Benito J. Gonzalez <bgonzalez@unicon.net>
2026-04-30 16:35:08 -0700

    chore: bump parent v50 -> v51 and migrate commons-lang -> commons-lang3
    uportal-portlet-parent:51 closes CVE-2025-48924 by replacing
    commons-lang 2.6 (EOL, no upstream fix) with commons-lang3 3.20.0.
    Source imports rename from org.apache.commons.lang.* to
    org.apache.commons.lang3.* (lang3 is API-compatible).
    
    Changes:
    - pom.xml: <parent><version>50</version></> -> <version>51</version>;
      drop the local <dependencyManagement> entry for commons-lang
      (parent v51 provides commons-lang3 3.20.0); update the
      <dependency> declaration to commons-lang3.
    - 7 .java files: sed import lang -> lang3 (StringUtils only).

--- src/main/java/org/jasig/portlet/widget/links/ResourceLinkService.java
+++ src/main/java/org/jasig/portlet/widget/links/ResourceLinkService.java
@@ -28,7 +28,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 @Slf4j
 public class ResourceLinkService {
